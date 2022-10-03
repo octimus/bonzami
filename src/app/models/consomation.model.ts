1f4f83f676d6c96fafb4cea5325893d797baf04b
octimus <maoulidasaid74@gmail.com>
2022-10-03 17:46:40 +0300

    correction du bug de l'affichage au changement de la date

--- src/app/models/consomation.model.ts
+++ src/app/models/consomation.model.ts
@@ -2,9 +2,28 @@ export class ConsomationModel{
     Essence:any;
     Gasoil:any;
     Petrole:any;
+
+    constructor(Essence:any,Gasoil:any,Petrole:any) {
+        this.Essence = Essence;
+        this.Gasoil = Gasoil;
+        this.Petrole = Petrole;
+    }
+
+    static fromMap(m: any): ConsomationModel {
+        return new ConsomationModel(m['Essence'], m['Gasoil'], m['Petrole']);
+    }
 }
 
 export class ConsomationResponse {
     consomTickets: ConsomationModel;
     consomVehicules: ConsomationModel;
+
+    constructor(consomTickets: any, consomVehicules:any){
+        this.consomTickets = consomTickets;
+        this.consomVehicules = consomVehicules;
+    }
+
+    static fromMap(m: any): ConsomationResponse {
+        return new ConsomationResponse(ConsomationModel.fromMap(m['consomTickets']), ConsomationModel.fromMap(m['consomVehicules']));
+    }
 }
--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConsomationResponse } from 'src/app/models/consomation.model';
 import { ApiKinuService } from 'src/app/services/api-kinu/api-kinu.service';
-import { finalize } from 'rxjs/operators';
+import { finalize, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-history',
@@ -12,18 +12,22 @@ export class HistoryPage implements OnInit {
 
   public consomations: ConsomationResponse;
   public loading: boolean = false;
-  public date: any = new Date().toISOString().split('T')[0];
+  public date: any = new Date().toISOString();
 
   constructor(private api: ApiKinuService) { }
   refresh(p: any){
     this.ngOnInit();
   }
   ngOnInit() {
+    this.consomations = null;
     this.loading = true;
-    this.api.getData(`/api/ticket/history/${this.date}`, {}).pipe(finalize(()=>{this.loading = false}))
+    this.api.getData(`/api/ticket/history/${this.date.split('T')[0]}`, {})
+    .pipe(finalize(()=>{this.loading = false}), take(1))
     .subscribe((data) => {
-      this.consomations = data;
+        if(data.data != ''){
+          this.consomations = ConsomationResponse.fromMap(typeof(data.data) == 'string' ? JSON.parse(data.data) : data.data);
+        }
+        console.log(data);
     });
   }
-
 }
